b9e36efe20fd72e9415833f6b27e10210a40bc48
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2007-12-01 22:00:19 +0000

    Fixing bug #1850  It was only an omission in the example but well
    git-svn-id: http://svn.php.net/repository/pear/packages/XML_HTMLSax/trunk@247425 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/HTMLtoXHTML.php
+++ docs/examples/HTMLtoXHTML.php
@@ -3,29 +3,31 @@
  * $Id$
  * Demonstrates conversion of HTML to XHTML
  */
-require_once('XML/HTMLSax3.php');
+require_once 'XML/HTMLSax3.php';
 
-class HTMLtoXHTMLHandler {
+class HTMLtoXHTMLHandler
+{
     var $xhtml;
     var $inTitle;
     var $pCounter;
 
-    function HTMLtoXHTMLHandler(){
-        $this->xhtml='';
+    function HTMLtoXHTMLHandler()
+    {
+        $this->xhtml = '';
         $this->inTitle = false;
-        $this->pCounter=0;
+        $this->pCounter = 0;
     }
 
     // Handles the writing of attributes - called from $this->openHandler()
-    function writeAttrs ($attrs) {
-        if ( is_array($attrs) ) {
-            foreach ( $attrs as $name => $value ) {
-
+    function writeAttrs ($attrs)
+    {
+        if (is_array($attrs)) {
+            foreach ($attrs as $name => $value) {
                 // Watch for 'checked'
-                if ( $name == 'checked' ) {
+                if ($name == 'checked') {
                     $this->xhtml.=' checked="checked"';
                 // Watch for 'selected'
-                } else if ( $name == 'selected' ) {
+                } else if ($name == 'selected') {
                     $this->xhtml.=' selected="selected"';
                 } else {
                     $this->xhtml.=' '.$name.'="'.$value.'"';
@@ -35,72 +37,86 @@ class HTMLtoXHTMLHandler {
     }
 
     // Opening tag handler
-    function openHandler(& $parser,$name,$attrs) {
-        if ( (isset ( $attrs['id'] ) && $attrs['id'] == 'title') || $name == 'title' )
-            $this->inTitle=true;
+    function openHandler(&$parser, $name, $attrs)
+    {
+        if ((isset($attrs['id']) && $attrs['id'] == 'title') || $name == 'title') {
+            $this->inTitle = true;
+        }
 
-        switch ( $name ) {
+        switch ($name) {
+            case 'input':
+                $this->xhtml .= '<input';
+                $this->writeAttrs($attrs);
+                $this->xhtml .= " />\n";
+                break;
+            case 'img':
+                $this->xhtml .= '<img';
+                $this->writeAttrs($attrs);
+                $this->xhtml .= " />\n";
+                break;
             case 'br':
-                $this->xhtml.="<br />\n";
+                $this->xhtml .= "<br />\n";
                 break;
             case 'html':
-                $this->xhtml.="<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"eng\">\n";
+                $this->xhtml .= "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"eng\">\n";
                 break;
             case 'p':
-                if ( $this->pCounter != 0 ) {
+                if ($this->pCounter != 0) {
                     $this->xhtml.="</p>\n";
                 }
-                $this->xhtml.="<p>";
+                $this->xhtml .= '<p>';
                 $this->pCounter++;
                 break;
             default:
-                $this->xhtml.="<".$name;
+                $this->xhtml .= '<'.$name;
                 $this->writeAttrs($attrs);
-                $this->xhtml.=">\n";
+                $this->xhtml .= ">\n";
                 break;
         }
-
     }
 
     // Closing tag handler
-    function closeHandler(& $parser,$name) {
-        if ( $this->inTitle ) {
-            $this->inTitle=false;
+    function closeHandler(&$parser, $name)
+    {
+        if ($this->inTitle) {
+            $this->inTitle = false;
         }
-        if ($name == 'body' && $this->pCounter != 0)
-            $this->xhtml.="</p>\n";
+        if ($name == 'body' && $this->pCounter != 0) {
+            $this->xhtml .= "</p>\n";
+        }
 
-        $this->xhtml.="</".$name.">\n";
+        $this->xhtml .= "</".$name.">\n";
     }
 
     // Character data handler
-    function dataHandler(& $parser,$data) {
-        if ( $this->inTitle )
-            $this->xhtml.='This is XHTML 1.0';
-        else
-            $this->xhtml.=$data;
+    function dataHandler(&$parser, $data)
+    {
+        $this->xhtml .= $this->inTitle ? 'This is XHTML 1.0' : $data;
     }
 
     // Escape handler
-    function escapeHandler(& $parser,$data) {
-        if ( $data == 'doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN"' )
+    function escapeHandler(&$parser, $data)
+    {
+        if ($data == 'doctype html public "-//W3C//DTD HTML 4.0 Transitional//EN"') {
             $this->xhtml.='<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+        }
     }
 
     // Return the XHTML document
-    function getXHTML () {
+    function getXHTML ()
+    {
         return $this->xhtml;
     }
 }
 
 // Get the HTML file
-$doc=file_get_contents('example.html');
+$doc = file_get_contents('example.html');
 
 // Instantiate the handler
-$handler=& new HTMLtoXHTMLHandler();
+$handler =& new HTMLtoXHTMLHandler();
 
 // Instantiate the parser
-$parser=& new XML_HTMLSax3();
+$parser =& new XML_HTMLSax3();
 
 // Register the handler with the parser
 $parser->set_object($handler);
@@ -113,5 +129,4 @@ $parser->set_escape_handler('escapeHandler');
 // Parse the document
 $parser->parse($doc);
 
-echo ( $handler->getXHTML() );
-?>
+echo $handler->getXHTML();
